0fe8723ba2a68925d24b5fb562cb77d4084eda98
williamreisac22-hub <williamreisac22@gmail.com>
2026-09-09 19:35:46 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE='magyver-eventos-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
+  }).catch(()=>caches.match('./index.html'))));
+});
